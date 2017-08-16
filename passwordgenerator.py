85fa3e5d2397716abd34f7baca80744c2d401cc8
daggerly <dagger003@163.com>
2017-08-16 21:21:27 +0800

    first commit

---
+++ passwordgenerator.py
@@ -0,0 +1,87 @@
+#coding:utf-8
+from datetime import date, timedelta, datetime
+from itertools import product as iter_product
+
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALPHABET = LOWER_LETTERS + UPPER_LETTERS
+DIGITS = '0123456789'
+NORMAL = ALPHABET + DIGITS
+PUNCTUATION = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+VISIBLE = ALPHABET + DIGITS + PUNCTUATION
+ASCII_LETTERS = [chr(i) for i in range(0xff+1)] 
+
+class PasswordGenerator(object):
+    def product(self, *iterables, **kw):
+        repeat = kw.get('repeat', 1)
+        for i in iter_product(*iterables, repeat=repeat):
+            yield ''.join(i)
+
+    def product_digits(self, length):
+        return self.product(DIGITS, repeat=length)
+
+    def product_visible_letters(self, length):
+        return self.product(VISIBLE, repeat=length)
+
+    def product_all_letters(self, length):
+        return self.product(ASCII_LETTERS, repeat=length)
+
+    def product_letters(self, pass_format):
+        """
+            using hashcat password format with additional ?p?n
+            ?l = abcdefghijklmnopqrstuvwxyz
+            ?u = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+            ?d = 0123456789
+            ?s = «space»!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+            ?a = ?l?u?d?s
+            ?b = 0x00 - 0xff
+            ?p = ?l?u
+            ?n = ?l?u?d
+            format: [{types}]{number}[{type}]{number}...
+            type: a -> ascii_letters
+                  d -> digits
+                  p -> punctution
+                  l -> letters(a+d+p)
+            number: repeat time
+            example:
+                  [d]8: 8 bytes digit(12345678)
+                  [a]3[d]3: 3 bytes ascii_letters + 3 bytes digits(xxx123)
+                  [ad]5: 5 bytes asccii_letters or digits(a34Fe)
+        """
+        return self.product(letters, repeat=length)
+
+    def product_birthday(self, start_date='19700101', end_date=None):
+        d = start_date = datetime.strptime(start_date, '%Y%m%d').date()
+        if end_date:
+            end_date = datetime.strptime(end_date, '%Y%m%d').date()
+        else:
+            end_date = date.today() 
+        day1 = timedelta(days=1)
+        while d<=end_date:
+            yield d.strftime('%Y%m%d')
+            d += day1
+        
+    def product_date(self):
+        # choose 2000 for there is 20000229
+        d = date(2000,1,1)
+        end_date = date(2000,12,31)
+        day1 = timedelta(days=1)
+        while d<=end_date:
+            yield d.strftime('%m%d')
+            d += day1
+
+    def product_chinesename_shortcut(self):        
+        name_alphabet = ALPHABET.replace('u', '').replace('U', '')\
+                                .replace('v', '').replace('V', '')\
+                                .replace('e', '').replace('E', '')\
+                                .replace('i', '').replace('I', '')
+        chars = list(p.product(name_alphabet, repeat=3))
+        for i in p.product(chars):
+            yield i
+        chars = list(p.product(name_alphabet, repeat=2))
+        for i in p.product(chars):
+            yield i
+
+p = PasswordGenerator()
+for i in p.product_chinesename_shortcut():
+    print i
